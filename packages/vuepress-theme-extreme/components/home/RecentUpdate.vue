6850c247936a0e5597b23934a354b2b7bf8fc3c2
kisstar <dwh.chn@foxmail.com>
2020-04-18 19:50:19 +0800

    feat($theme-extreme): create the basis for the latest updated module

---
+++ packages/vuepress-theme-extreme/components/home/RecentUpdate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="recent-update">
+    <div class="title">最近更新</div>
+    <Timeline />
+  </div>
+</template>
+
+<script>
+import Timeline from '../Timeline.vue';
+
+export default {
+  name: 'RecentUpdate',
+  components: {
+    Timeline,
+  },
+};
+</script>
--- packages/vuepress-theme-extreme/components/home/index.vue
+++ packages/vuepress-theme-extreme/components/home/index.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="home">
     <Banner :bgImage="$withBase($page.frontmatter.bannerBg)" />
+    <RecentUpdate />
   </main>
 </template>
 
 <script>
 import Banner from './Banner.vue';
+import RecentUpdate from './RecentUpdate.vue';
 
 export default {
   name: 'Home',
-  components: { Banner },
+  components: { Banner, RecentUpdate },
 };
 </script>

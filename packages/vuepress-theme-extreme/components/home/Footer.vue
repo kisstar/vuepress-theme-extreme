71a0e5700dd327f6be026562de4ac422a62c9be8
kisstar <dwh.chn@foxmail.com>
2020-05-02 14:50:50 +0800

    feat($theme-extreme): create footer component

---
+++ packages/vuepress-theme-extreme/components/home/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer">
+    <div class=" copyright-container">
+      <p>
+        vuepress-theme-extreme developed by <NavLink :item="linkInfo.kisstar" /> & Powered by
+        <NavLink :item="linkInfo.vuepress" />.
+      </p>
+      <p>Copyright © 2020. All Rights Reserved.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavLink from '../NavLink.vue';
+
+const linkInfo = Object.freeze({
+  kisstar: {
+    text: 'Kisstar',
+    link: 'https://github.com/kisstar/',
+    rel: 'noopener noreferrer',
+    target: '_blank',
+  },
+  vuepress: {
+    text: 'VuePress',
+    link: 'https://vuepress.vuejs.org/',
+    rel: 'noopener noreferrer',
+    target: '_blank',
+  },
+});
+
+export default {
+  name: 'Footer',
+  components: {
+    NavLink,
+  },
+  data() {
+    return {
+      linkInfo,
+    };
+  },
+};
+</script>
--- packages/vuepress-theme-extreme/components/home/index.vue
+++ packages/vuepress-theme-extreme/components/home/index.vue
@@ -2,15 +2,17 @@
   <main class="home">
     <Banner :bgImage="$withBase($page.frontmatter.bannerBg)" />
     <RecentUpdate />
+    <Footer />
   </main>
 </template>
 
 <script>
 import Banner from './Banner.vue';
 import RecentUpdate from './RecentUpdate.vue';
+import Footer from './Footer.vue';
 
 export default {
   name: 'Home',
-  components: { Banner, RecentUpdate },
+  components: { Banner, RecentUpdate, Footer },
 };
 </script>

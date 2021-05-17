77a37212d318bb8617c296d58fd57f57799b701e
kisstar <dwh.chn@foxmail.com>
2021-05-18 00:30:48 +0800

    feat($theme-extreme): create infrastructure and styles for Latest

---
+++ packages/vuepress-theme-extreme/components/Latest.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="latest">
+    <li class="latest-item" v-for="item in data" :key="item.link">
+      <img class="thumbnail" v-if="item.thumbnail" :src="item.thumbnail" />
+      <div class="content">
+        <h3 class="title">{{ item.title }}</h3>
+        <p class="excerpt">{{ item.excerpt }}</p>
+        <p class="time">{{ item.time }}</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import VueTypes from '../lib/vue-types';
+
+export default {
+  name: 'Latest',
+  props: {
+    data: VueTypes.array.def(() => []),
+  },
+};
+</script>
--- packages/vuepress-theme-extreme/components/Timeline.vue
+++ packages/vuepress-theme-extreme/components/Timeline.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="timeline">
-    <li class="time-node" v-for="item in data" :key="item.src" :data-title="item.time">
+    <li class="time-node" v-for="item in data" :key="item.link" :data-title="item.time">
       <h3>{{ item.title }}</h3>
-      <img v-if="item.img" :src="item.img" />
-      <p>{{ item.des }}</p>
+      <img v-if="item.thumbnail" :src="item.thumbnail" />
+      <p>{{ item.excerpt }}</p>
     </li>
   </ul>
 </template>
@@ -12,7 +12,7 @@
 import VueTypes from '../lib/vue-types';
 
 export default {
-  name: 'RecentUpdate',
+  name: 'Timeline',
   props: {
     data: VueTypes.array.def(() => []),
   },

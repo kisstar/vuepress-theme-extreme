839f5689e227e37eb001537d98c2d3cb9c23f049
kisstar <dwh.chn@foxmail.com>
2021-05-21 08:29:24 +0800

    build: 引入 @vuepress/blog 插件

--- packages/vuepress-theme-extreme/index.js
+++ packages/vuepress-theme-extreme/index.js
@@ -7,8 +7,40 @@ const path = require('path');
 
 const resolve = (...p) => path.resolve(__dirname, '../', ...p);
 
-module.exports = () => {
-  return {
+/**
+ * @param {Object} themeConfig 用户对主题的配置，由当前主题处理实现
+ * @param {Object} ctx 编译期上下文
+ * @returns 主题本身的配置，由 VuePress 本身提供支持
+ */
+module.exports = (themeConfig, ctx) => {
+  const oThemeConfig = {
     globalLayout: resolve('layouts'),
+    ...themeConfig,
   };
+  const blogPluginOptions = {
+    directories: [
+      {
+        id: 'post',
+        dirname: 'posts',
+        path: '/',
+      },
+    ],
+    frontmatters: [
+      {
+        id: 'tag',
+        keys: ['tags'],
+        path: '/tag/',
+      },
+    ],
+    globalPagination: {
+      lengthPerPage: 10,
+    },
+  };
+  const plugins = [['@vuepress/blog', blogPluginOptions]];
+  const config = {
+    themeConfig: oThemeConfig,
+    plugins,
+  };
+
+  return config;
 };
